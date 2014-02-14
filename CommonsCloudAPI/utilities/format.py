18f5d9a64d9292e08b7cad4a88903a3ab1dd100d
Joshua Powell <joshua@viableindustries.com>
2014-02-14 15:13:50 -0500

    Updated the format class to have a serialize flag;

--- CommonsCloudAPI/utilities/format.py
+++ CommonsCloudAPI/utilities/format.py
@@ -110,7 +110,6 @@ class FormatContent:
       result = OrderedDict()
 
       for key in self.the_content.__mapper__.c.keys():
-
           result[key] = getattr(self.the_content, key)
 
       return result
