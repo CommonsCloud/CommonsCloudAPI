b1bf572e33bd31c300fc77f655cee9520efaaad5
Joshua Powell <joshua@viableindustries.com>
2014-02-25 15:45:46 -0500

    Added comments to the Template module;

--- CommonsCloudAPI/modules/template/models.py
+++ CommonsCloudAPI/modules/template/models.py
@@ -17,6 +17,16 @@ Import Application Dependencies
 from CommonsCloudAPI.extensions import db
 
 
+"""
+Template
+
+A template within CommonsCloudAPI is backbone of the system, allowing for
+organizations to collection data, a template defines the data model within
+the system.
+
+@param (object) db.Model
+    The model is the Base we use to define our database structure
+"""
 class Template(db.Model):
   
   id = db.Column(db.Integer, primary_key=True)
@@ -24,24 +34,22 @@ class Template(db.Model):
   help = db.Column(db.String(255))
   storage = db.Column(db.String(255))
   is_public = db.Column(db.Boolean)
-  crowd_sourcing = db.Column(db.Boolean)
-  moderate = db.Column(db.Boolean)
+  is_crowdsourced = db.Column(db.Boolean)
+  is_moderated = db.Column(db.Boolean)
   is_listed = db.Column(db.Boolean)
   created = db.Column(db.DateTime)
   status = db.Column(db.Boolean)
   # fields = db.relationship("Field", secondary=template_fields, backref=db.backref('templates'))
   # statistics = db.relationship("Statistic", backref=db.backref('statistics'))
 
-  def __init__(self, name, storage, owner, description="", publicly_viewable="", crowd_sourcing="", moderate="", applications=None):
+  def __init__(self, name="", help="", storage="", is_public="", is_crowdsourced="", is_moderated="", is_listed="", created=datetime.utcnow(), status=True):
     self.name = name
-    self.description = description
+    self.help = help
     self.storage = storage
-    self.owner = owner
-    self.publicly_viewable = publicly_viewable
-    self.crowd_sourcing = crowd_sourcing
-    self.moderate = moderate
-    self.created = datetime.utcnow()
-    self.applications = applications
-    self.is_listed = True
-    self.status = True
+    self.is_public = is_public
+    self.is_crowdsourced = is_crowdsourced
+    self.is_moderated = is_moderated
+    self.is_listed = is_listed
+    self.created = created
+    self.status = status
 
