28e33d3759794af310e2ca1b9a077370977f52da
Joshua Powell <joshua@viableindustries.com>
2014-02-26 11:51:22 -0500

    Created a new Base class for Commons Models to build on top of providing simple, reusable functionality like JSON/CSV responses and giving us a platform for expanding these commonly used methods/functionality down the road;

--- CommonsCloudAPI/models.py
+++ CommonsCloudAPI/models.py
@@ -1,14 +1,88 @@
-# """
-# For CommonsCloud copyright information please see the LICENSE document
-# (the "License") included with this software package. This file may not
-# be used in any manner except in compliance with the License
+"""
+For CommonsCloud copyright information please see the LICENSE document
+(the "License") included with this software package. This file may not
+be used in any manner except in compliance with the License
 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# """
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+"""
+For CommonsCloud copyright information please see the LICENSE document
+(the "License") included with this software package. This file may not
+be used in any manner except in compliance with the License
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+"""
+Import Flask Dependencies
+"""
+from flask import request
+
+
+"""
+Import Commons Cloud Dependencies
+"""
+from CommonsCloudAPI.extensions import status as status_
+
+from CommonsCloudAPI.utilities.format_csv import CSV
+from CommonsCloudAPI.utilities.format_json import JSON
+
+
+class CommonsModel(object):
+
+  def __init__(self):
+    pass
+
+  """
+  Create a valid response to be served to the user
+
+  @param (object) self
+
+  @param (object)/(list) the_content
+      A list of templates or a single template object to be delivered
+
+  @param (str) list_name
+      If the `the_content` is really a list then we should give int
+      a name for the list to be keyed as in the returned JSON object
+
+  @return (object) 
+      An JSON object either contianing the formatted content or an error
+      message describing why the content couldn't be delivered
+
+  """
+  def endpoint_response(self, the_content, list_name=''):
+
+    """
+    If the user is properly authenticated, then proceed to see if they
+    have requests a type of content we serve
+    """
+    if request.headers['Content-Type'] == 'application/json' or \
+        (hasattr(request.args, 'format') and request.args['format'] == 'json'):
+
+      this_data = JSON(the_content, serialize=True, list_name=list_name)
+      return this_data.create(), 200
+
+    elif request.headers['Content-Type'] == 'text/csv' or \
+        (hasattr(request.args, 'format') and request.args['format'] == 'csv'):
+
+      this_data = CSV(the_content, serialize=True)
+      return this_data.create(), 200
+
+    """
+    If the user hasn't requested a specific content type then we should
+    tell them that, by directing them to an "Unsupported Media Type"
+    """
+    return status_.status_415(), 415
+
 
 
 # """
