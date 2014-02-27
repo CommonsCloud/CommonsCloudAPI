13f4503fcf738d407a09cde65f3fc008957b6f4b
Joshua Powell <joshua@viableindustries.com>
2014-02-27 13:40:33 -0500

    Began migrating the models out of the Blueprints .. which we found out is the wrong way to do things anyway;

--- CommonsCloudAPI/errors.py
+++ CommonsCloudAPI/errors.py
@@ -30,7 +30,10 @@ def load_errorhandlers(app):
   them a little feedback. These should get better over time, but for now we can at least make
   it look like they aren't completely leaving the system and lost with default server error pages.
   """
-  @app.errorhandler(401)
+  @app.errorhandler(400)
+  def internal_error(error):
+    return status_.status_400(), 400
+
   def internal_error(error):
     return status_.status_401(), 401
 
