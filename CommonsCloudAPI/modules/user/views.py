0a7ef63c3c6437ea138dd1f05abf876776f98629
Joshua Powell <joshua@viableindustries.com>
2014-05-06 11:33:26 -0400

    Added preflight for user endpoints;"

--- CommonsCloudAPI/modules/user/views.py
+++ CommonsCloudAPI/modules/user/views.py
@@ -40,6 +40,10 @@ Basic route for currently logged in user
 def index():
   return redirect(url_for('user.user_profile_get')), 301
 
+@module.route('/v2/user.<string:extension>', methods=['OPTIONS'])
+def user_preflight(extension):
+    return status_.status_200(), 200
+
 
 @module.route('/v2/user/me.<string:extension>', methods=['OPTIONS'])
 def user_me_preflight(extension):
