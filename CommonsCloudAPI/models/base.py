161a52bf2fc9cb8c4bb1df1fa54c9e0e12951616
Joshua Powell <joshua@viableindustries.com>
2014-06-24 11:13:04 -0400

    Added updated to the list of default public fields;

--- CommonsCloudAPI/models/base.py
+++ CommonsCloudAPI/models/base.py
@@ -65,7 +65,7 @@ import geoalchemy2.functions as func
 
 class CommonsModel(object):
 
-  __public__ = ['id', 'created', 'status']
+  __public__ = ['id', 'created', 'status', 'updated']
   __public_relationships__ = None
 
   def __init__(self):
