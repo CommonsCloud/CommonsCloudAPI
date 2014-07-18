485f05640b24075ef69da109bce53be3f87f0de3
Joshua Powell <joshua@viableindustries.com>
2014-07-18 11:28:26 -0400

    Updated Caption, Credit, and Link fields for attachments;

--- CommonsCloudAPI/models/feature.py
+++ CommonsCloudAPI/models/feature.py
@@ -489,6 +489,7 @@ class Feature(CommonsModel):
             # Step 1: Create a record in the attachment_ so that we have
             #         an ID for our attachment
             #              
+            logger.warning('file_', file_)
             attachment_details = {
               'caption': sanitize.sanitize_string(file_.caption),
               'credit': sanitize.sanitize_string(file_.credit),
