69cb3fbd39a8677ef95cb36cc12c97c216bb6197
Joshua Powell <joshua@viable.io>
2014-12-05 12:34:01 -0500

    Changed the log message for Feature Notifications

--- CommonsCloudAPI/models/feature.py
+++ CommonsCloudAPI/models/feature.py
@@ -367,8 +367,8 @@ class Feature(CommonsModel):
         """
         if not bulk:
           feature_json = self.serialize_object(self.feature_get(storage, new_feature.id))
-          logger.info('A new feature was created in %s with an id of %d %s', 
-              storage, new_feature.id, type(feature_json))
+          logger.info('A new feature was created in %s with an id of %d', 
+              storage, new_feature.id)
           trigger_feature_created.send(current_app._get_current_object(),
                                storage=storage, template=Template_, feature=new_feature, feature_json=feature_json)
         return new_feature
