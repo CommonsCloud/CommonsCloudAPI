41aa13b8288cda3ee3329aa508c94133ae12790d
Joshua Powell <joshua@viable.io>
2015-03-15 15:57:25 -0400

    Added messages to both the Telephone and Address User > Endpoints explaining why these two models should not be opened up to direct REST access;

---
+++ CommonsCloudAPI/modules/user/endpoints/telephone/__init__.py
@@ -0,0 +1,21 @@
+"""
+For CommonsCloud copyright information please see the LICENSE document
+(the "License") included with this software package. This file may not
+be used in any manner except in compliance with the License
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+"""
+Telephone numbers should never be accessed directly. They should only, ever be
+accessed through another entity such as a `User` or `Organization` object.
+
+Because of this no GET, POST, PATCH, PUT, or DELETE functionality has been
+added to this endpoint.
+
+This file serves as a reminder that we do not want to open these up directly.
+"""
